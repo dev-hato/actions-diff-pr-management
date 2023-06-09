d259447b9c16356e7de47e409fa97d85a8f6d22e
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2023-06-09 14:57:21 +0900

    READMEに引数のデフォルト値追加 (#1052)
    * READMEに引数のデフォルト値追加
    
    * undefinedの場合はデフォルト値を記述しない
    
    * README修正 (#1053)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/update_readme/update_readme/get_inputs_markdown.js
+++ scripts/update_readme/update_readme/get_inputs_markdown.js
@@ -4,13 +4,19 @@ const yaml = require('js-yaml')
 module.exports = () => {
   const ymlFile = yaml.load(fs.readFileSync('action.yml', 'utf8'))
   const inputs = ymlFile.inputs
-  const rows = ['| 引数名 | 説明 | 必須 |', '|:---:|:---:|:---:|']
+  const rows = ['| 引数名 | 説明 | 必須 | デフォルト値 |', '|:---:|:---:|:---:|:--:|']
 
   for (const inputName of Object.keys(inputs)) {
     let row = `| ${inputName} | ${inputs[inputName].description} | `
 
     if (inputs[inputName].default === undefined && inputs[inputName].required) {
       row += 'O'
+    }
+
+    row += ' | '
+
+    if (inputs[inputName].default !== undefined) {
+      row += inputs[inputName].default
     }
 
     row += ' |'
