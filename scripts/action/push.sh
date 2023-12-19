fcabb976361df98a31bedc307f6be4be35c224bf
massongit <suzukimasaya428@gmail.com>
2023-12-19 15:24:34 +0900

    git commit, push時に--no-verifyを付与するオプション追加

--- scripts/action/push.sh
+++ scripts/action/push.sh
@@ -3,9 +3,24 @@
 git config user.name "github-actions[bot]"
 EMAIL="41898282+github-actions[bot]@users.noreply.github.com"
 git config user.email "${EMAIL}"
+GIT_COMMIT_COMMAND="git commit "
+
+if [ "$NO_VERIFY" = "true" ]; then
+  GIT_COMMIT_COMMAND+="--no-verify "
+fi
+
+GIT_COMMIT_COMMAND+="-m \"${PR_TITLE_PREFIX}\""
+eval "$GIT_COMMIT_COMMAND"
 git commit -m "${PR_TITLE_PREFIX}"
 REPO_URL="https://"
 REPO_URL+="${AUTHOR}:${TOKEN}@github.com/"
 REPO_URL+="${REPOSITORY}.git"
 GITHUB_HEAD="HEAD:refs/heads/${BRANCH_NAME_PREFIX}-${HEAD_REF}"
-git push -f "${REPO_URL}" "${GITHUB_HEAD}"
+GIT_PUSH_COMMAND="git push "
+
+if [ "$NO_VERIFY" = "true" ]; then
+  GIT_PUSH_COMMAND+="--no-verify "
+fi
+
+GIT_PUSH_COMMAND=+"-f \"${REPO_URL}\" \"${GITHUB_HEAD}\""
+eval "$GIT_PUSH_COMMAND"
