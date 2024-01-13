40e65038ab238f87b7b71c40f4d54d0d50760047
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2024-01-14 03:35:22 +0900

    ブランチ名に「#」が含まれる際はMermaidに含まないようにする (#1443)
    * ブランチ名に「#」が含まれる際はMermaidに含まないようにする
    
    * replaceAllを使う
    
    * fix style
    
    * Mermaid以外ではエスケープしないブランチ名を使う
    
    * Mermaid以外ではエスケープしないブランチ名を使う
    
    * 余分な変更を戻す
    
    * 余分な変更を戻す

--- scripts/action/create_pull_request.js
+++ scripts/action/create_pull_request.js
@@ -1,5 +1,6 @@
 module.exports = async ({ github, context }) => {
   const HEAD_REF = process.env.HEAD_REF
+  const escapedHeadRef = HEAD_REF.replaceAll('#', '')
   const PR_NUMBER = process.env.PR_NUMBER
   const PR_TITLE_PREFIX = process.env.PR_TITLE_PREFIX
   let head = process.env.BRANCH_NAME_PREFIX
@@ -8,18 +9,19 @@ module.exports = async ({ github, context }) => {
     head += '-' + HEAD_REF
   }
 
+  const escapedHead = head.replaceAll('#', '')
   const headWithRepo = process.env.ORG_NAME + ':' + head
   let title = PR_TITLE_PREFIX
   let body = process.env.PR_DESCRIPTION_PREFIX
 
-  body += `本PR ( \`${head}\` ) をマージすると差分が次のPRに反映されます。\n`
+  body += `本PR ( \`${escapedHead}\` ) をマージすると差分が次のPRに反映されます。\n`
   body += '* '
 
   if (PR_NUMBER !== '') {
     body += `#${PR_NUMBER} ( `
   }
 
-  body += `\`${HEAD_REF}\``
+  body += `\`${escapedHeadRef}\``
 
   if (PR_NUMBER !== '') {
     body += ' )'
@@ -27,15 +29,15 @@ module.exports = async ({ github, context }) => {
 
   body += '\n'
   body += '```mermaid\n'
-  body += `%%{init: {'gitGraph': {'mainBranchName': '${HEAD_REF}'}}}%%\n`
+  body += `%%{init: {'gitGraph': {'mainBranchName': '${escapedHeadRef}'}}}%%\n`
   body += 'gitGraph\n'
 
   for (let i = 0; i < 2; i++) {
     body += '  commit\n'
   }
 
-  body += `  branch ${head}\n`
-  body += `  checkout ${head}\n`
+  body += `  branch ${escapedHead}\n`
+  body += `  checkout ${escapedHead}\n`
   let commit = PR_TITLE_PREFIX
 
   if (commit.length > 6) {
@@ -43,8 +45,8 @@ module.exports = async ({ github, context }) => {
   }
 
   body += `  commit id: "${commit}"\n`
-  body += `  checkout ${HEAD_REF}\n`
-  body += `  merge ${head}\n`
+  body += `  checkout ${escapedHeadRef}\n`
+  body += `  merge ${escapedHead}\n`
   body += '```'
 
   if (PR_NUMBER !== '') {
