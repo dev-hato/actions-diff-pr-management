7cc8ab103917849f6995064afae9f4cbcfd009ae
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2024-09-13 07:18:57 +0900

    処理内容を説明するコメント追加 (#1687)
    * 処理内容を説明するコメント追加
    
    * format修正のPR -> 修正PR

--- scripts/action/close_pull_request.js
+++ scripts/action/close_pull_request.js
@@ -10,6 +10,8 @@ module.exports = async ({ github, context }) => {
     owner: context.repo.owner,
     repo: context.repo.repo,
   };
+
+  // 修正PRの情報を取得する
   const pullsListParams = {
     head: context.repo.owner + ":" + headName,
     state: "open",
@@ -19,6 +21,7 @@ module.exports = async ({ github, context }) => {
   const pulls = await github.paginate(github.rest.pulls.list, pullsListParams);
 
   for (const pull of pulls) {
+    // 修正PRをcloseする (修正PRのstateをclosedに更新する)
     const pullsUpdateParams = {
       pull_number: pull.number,
       state: "closed",
@@ -26,6 +29,8 @@ module.exports = async ({ github, context }) => {
     };
     console.log("call pulls.update:", pullsUpdateParams);
     await github.rest.pulls.update(pullsUpdateParams);
+
+    // 修正PRのブランチを削除する
     const gitDeleteRefParams = {
       ref: "heads/" + headName,
       ...commonParams,
