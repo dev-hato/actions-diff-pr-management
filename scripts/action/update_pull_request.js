8b7da526ea191cae999f22441d77499261118cb3
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-09-20 00:22:40 +0900

    fix

--- scripts/action/update_pull_request.js
+++ scripts/action/update_pull_request.js
@@ -1,39 +1,21 @@
 const generateTitleDescription = require("./generate_title_description.js");
+const getPullRequests = require('./get_pull_requests.js');
 
 module.exports = async ({ github, context }) => {
-  const HEAD_REF = process.env.HEAD_REF;
-  let headName = process.env.BRANCH_NAME_PREFIX;
-
-  if (HEAD_REF !== "") {
-    headName += "-" + HEAD_REF;
-  }
-
   const { title, body } = generateTitleDescription();
-  const commonParams = {
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-  };
 
-  // 修正PRの情報を取得する
-  const pullsListParams = {
-    head: context.repo.owner + ":" + headName,
-    state: "open",
-    ...commonParams,
-  };
-  console.log("call pulls.list:", pullsListParams);
-  const pulls = await github.paginate(github.rest.pulls.list, pullsListParams);
-
-  for (const pull of pulls) {
+  for (const pull of await getPullRequests({github, context})) {
     if (pull.title === title && pull.body === body) {
       continue;
     }
 
     // PRのタイトルやDescriptionを更新する
     const pullsUpdateParams = {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
       pull_number: pull.number,
       title,
       body,
-      ...commonParams,
     };
     console.log("call pulls.update:", pullsUpdateParams);
     await github.rest.pulls.update(pullsUpdateParams);
