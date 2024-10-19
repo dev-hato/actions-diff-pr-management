01811f2a996a6beb19aa17d689bea6f04b67970c
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-19 20:35:37 +0900

    CIで使用しているスクリプトをTypeScript化する

--- scripts/action/get_pull_requests.ts
+++ scripts/action/get_pull_requests.ts
@@ -1,4 +1,13 @@
-module.exports = async ({ github, context, additionalParams }) => {
+import type {GitHub} from "@actions/github/lib/utils";
+import type {Context} from "@actions/github/lib/context";
+import type { PaginatingEndpoints } from "@octokit/plugin-paginate-rest";
+import type {RestEndpointMethodTypes} from "@octokit/plugin-rest-endpoint-methods";
+
+export async function getPullRequests(
+    github: InstanceType<typeof GitHub>,
+    context: Context,
+    base?: string
+):Promise<PaginatingEndpoints["GET /repos/{owner}/{repo}/pulls"]["response"]["data"]>{
   const HEAD_REF = process.env.HEAD_REF;
   let head = context.repo.owner + ":" + process.env.BRANCH_NAME_PREFIX;
 
@@ -6,13 +15,13 @@ module.exports = async ({ github, context, additionalParams }) => {
     head += "-" + HEAD_REF;
   }
 
-  const pullsListParams = {
+  const pullsListParams:RestEndpointMethodTypes["pulls"]["list"]["parameters"] = {
     owner: context.repo.owner,
     repo: context.repo.repo,
     head,
     state: "open",
-    ...additionalParams,
+    base,
   };
   console.log("call pulls.list:", pullsListParams);
   return await github.paginate(github.rest.pulls.list, pullsListParams);
-};
+}
--- scripts/action/update_pull_request.ts
+++ scripts/action/update_pull_request.ts
@@ -1,16 +1,22 @@
-const generateTitleDescription = require("./generate_title_description.js");
-const getPullRequests = require("./get_pull_requests.js");
+import type { Context } from "@actions/github/lib/context";
+import type { GitHub } from "@actions/github/lib/utils";
+import type {RestEndpointMethodTypes} from "@octokit/plugin-rest-endpoint-methods";
+import {generateTitleDescription} from "./generate_title_description";
+import {getPullRequests} from "./get_pull_requests";
 
-module.exports = async ({ github, context }) => {
+export async function script(
+    github: InstanceType<typeof GitHub>,
+    context: Context,
+) {
   const { title, body } = generateTitleDescription();
 
-  for (const pull of await getPullRequests({ github, context })) {
+  for (const pull of await getPullRequests(github, context)) {
     if (pull.title === title && pull.body === body) {
       continue;
     }
 
     // PRのタイトルやDescriptionを更新する
-    const pullsUpdateParams = {
+    const pullsUpdateParams:RestEndpointMethodTypes["pulls"]["update"]["parameters"] = {
       owner: context.repo.owner,
       repo: context.repo.repo,
       pull_number: pull.number,
@@ -20,4 +26,4 @@ module.exports = async ({ github, context }) => {
     console.log("call pulls.update:", pullsUpdateParams);
     await github.rest.pulls.update(pullsUpdateParams);
   }
-};
+}
