9457654c4f8beae91a3942028135287071b6c0ce
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-21 00:32:18 +0900

    fix

--- src/set_git_command.sh
+++ src/set_git_command.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 {
-  echo "GIT_COMMIT_COMMAND=$GIT_COMMIT_COMMAND --no-verify"
-  echo "GIT_PUSH_COMMAND=$GIT_PUSH_COMMAND --no-verify"
-} >> "$GITHUB_ENV"
+	echo "GIT_COMMIT_COMMAND=$GIT_COMMIT_COMMAND --no-verify"
+	echo "GIT_PUSH_COMMAND=$GIT_PUSH_COMMAND --no-verify"
+} >>"$GITHUB_ENV"
