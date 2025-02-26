1fbcd8e1c37c9dc705735a9b2568cd7b3997fd51
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-02-27 06:59:55 +0900

    Fix PATH

--- scripts/super_linter/build/set_path.sh
+++ scripts/super_linter/build/set_path.sh
@@ -4,4 +4,4 @@ npm ci
 tag_name="$(yq '.jobs.build.steps[-1].uses' .github/workflows/super-linter.yml | sed -e 's;/slim@.*;:slim;g')"
 tag_version="$(yq '.jobs.build.steps[-1].uses | line_comment' .github/workflows/super-linter.yml)"
 PATH="$(docker run --rm --entrypoint '' "ghcr.io/${tag_name}-${tag_version}" /bin/sh -c 'echo $PATH')"
-echo "PATH=/github/workspace/node_modules/.bin:${PATH}" >>"$GITHUB_ENV"
+echo "PATH=/github/workspace/node_modules:${PATH}" >>"$GITHUB_ENV"
