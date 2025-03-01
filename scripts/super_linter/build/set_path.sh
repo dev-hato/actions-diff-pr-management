ee2f2e9ddf86fa90d7312eee9a54dcf9b6aba8dc
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-03-01 12:40:13 +0900

    Update set_path.sh

--- scripts/super_linter/build/set_path.sh
+++ scripts/super_linter/build/set_path.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-npm ci
+npm ci --dev
 tag_name="$(yq '.jobs.build.steps[-1].uses' .github/workflows/super-linter.yml | sed -e 's;/slim@.*;:slim;g')"
 tag_version="$(yq '.jobs.build.steps[-1].uses | line_comment' .github/workflows/super-linter.yml)"
 PATH="$(docker run --rm --entrypoint '' "ghcr.io/${tag_name}-${tag_version}" /bin/sh -c 'echo $PATH')"
